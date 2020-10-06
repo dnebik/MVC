22656a3c23eb384d35f39824f3faf3e2d898abf9
ya.dneb@yandex.ru <ya.dneb@yandex.ru>
2020-10-06 05:51:45 +0300

    хранение title

--- app/core/view.php
+++ app/core/view.php
@@ -2,13 +2,14 @@
 class View
 {
     public $controller_name;
+    public $title = 'MVC';
 
     public function __construct($controller_name)
     {
         $this->controller_name = $controller_name;
     }
 
-    public function render($content_view, $template_view, $data = null)
+    public function render($content, $template_view, $data = null)
     {
         if (is_array($data))
         {
